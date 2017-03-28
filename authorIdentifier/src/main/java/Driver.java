7bf0351c8cd6f3d0f31eff37794262c678c08a9c
Ktailor34 <tailorkishan123@gmail.com>
2017-03-28 10:35:07 -0400

    Commit 3, added file IO, and different words method

--- authorIdentifier/src/main/java/Driver.java
+++ authorIdentifier/src/main/java/Driver.java
@@ -1,15 +1,21 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by s738110 on 3/15/17.
  */
 public class Driver {
 
-	public static void main (String args[]) {
+
+	public static void main(String args[]) {
+
 
 		Scanner lineScanner = new Scanner(System.in);
-		String line;
+		String line = null;
 		ArrayList test = new ArrayList();
 		boolean done = false;
 		boolean done1 = false;
@@ -19,76 +25,86 @@ public class Driver {
 		//ArrayList Hannah = new ArrayList();
 		//Hannah.add("a");
 		//Grace.add("a");
-		String[] Hannah = {"q", "w", "e", "e", "t", "y"};
-		String[] Standard = {"q", "w", "e", "e", "t", "y"};
+		//String[] Hannah = {"q", "w", "e", "e", "t", "y"};
+		//String[] Standard = {"q", "w", "e", "e", "t", "y"};
 		ArrayList unique = new ArrayList();
 
 
-		while (lineScanner.hasNextLine() && done1 != true) {
-			while (lineScanner.hasNextLine() && done != true) {
-				// Takes in entire string
-				line = lineScanner.nextLine();
-				// Removes punctuation
+		try {
+			String fileName = "File1.txt";
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			while ((line = bufferedReader.readLine()) != null) {
 				line = line.replaceAll("[^a-zA-Z]", " ").toLowerCase();
-				//Tokens string to allow to read one word at a time
-				Scanner token = new Scanner(line);
-				//splits tokened setring
-				while (token.hasNextLine()) {
-					authorList.add(token.next());
-				}
-
-
-
-				if (line.equals("done")) {
-					System.out.println(authorList);
-					done = true;
+				for (String word : line.split(" ")) {
+					authorList.add(word);
 				}
 			}
-			while (lineScanner.hasNextLine() && done1 != true) {
-				// Takes in entire string
-				line = lineScanner.nextLine();
-				// Removes punctuation
-				line = line.replaceAll("[^a-zA-Z]", " ").toLowerCase();
-				//Tokens string to allow to read one word at a time
-				Scanner token = new Scanner(line);
-				//splits tokened setring
-				while (token.hasNextLine()) {
-					author1List.add(token.next());
-				}
 
-				if (line.equals("done")) {
-					System.out.println(authorList);
-					System.out.println(author1List);
-					done1 = true;
-				}
-			}
+
+			// Takes in entire string
+			//line = lineScanner.nextLine();
+			// Removes punctuation
+			//Tokens string to allow to read one word at a time
+			//Scanner token = new Scanner(line);
+			//splits tokened setring
+			/*while (token.hasNextLine()) {
+				authorList.add(token.next());
+			}*/
+
+		} catch (FileNotFoundException ex) {
+			System.out.println("File not found");
+		} catch (IOException ex) {
+			System.out.println("IO Exception");
 		}
-		test = compareCommon(authorList, author1List);
+
+		try {
+			String fileName = "File2.txt";
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			while ((line = bufferedReader.readLine()) != null) {
+				line = line.replaceAll("[^a-zA-Z]", " ").toLowerCase();
+				System.out.println(line);
+
+				for (String word : line.split(" ")) {
+					authorList.add(word);
+				}
+			}
+
+		} catch (FileNotFoundException ex) {
+			System.out.println("File not found");
+		} catch (IOException ex) {
+			System.out.println("IO Exception");
+		}
+
+		test = findUnique(authorList);
 		System.out.println(test);
 	}
 
-	public static ArrayList compareCommon(ArrayList list, ArrayList list2){
+
+	public static ArrayList compareCommon(ArrayList list, ArrayList list2) {
 
 		//Puts words into array and compares similar words
 		ArrayList common = new ArrayList();
-		for(int i = 0; i <= list.size()-1; i++) {
-			for(int k = 0; k <= list2.size()-1; k++) {
-				if (list.get(k).equals(list2.get(i)) && !common.contains(list.get(k))) {
+		for (int i = 0; i <= list.size() - 1; i++) {
+			for (int k = 0; k <= list2.size() - 1; k++) {
+				if (list.get(i).equals(list2.get(k)) && !common.contains(list.get(k))) {
 
 					common.add(list.get(k));
-
 				}
 			}
 		}
 		return common;
 	}
 
-	public static ArrayList findUnique(ArrayList list){
+	public static ArrayList findUnique(ArrayList list) {
 
 		//Puts words into array and compares similar words
 		ArrayList unique = new ArrayList();
-		for(int i = 0; i <= list.size()-1; i++) {
-			for(int k = 0; k <= list.size()-1; k++) {
+		for (int i = 0; i <= list.size() - 1; i++) {
+			for (int k = 0; k <= list.size() - 1; k++) {
 				if (!unique.contains(list.get(k))) {
 
 					unique.add(list.get(k));
@@ -100,5 +116,20 @@ public class Driver {
 
 	}
 
+	public static ArrayList compareDifferentiatingWords(ArrayList list, ArrayList list2) {
+		//puts words into arraylist and finds the words that one list contains that the other doesn't
+		ArrayList compareDifferentiatingWords = new ArrayList();
 
+		for (int i = 0; i <= list.size() - 1; i++) {
+			for (int k = 0; k <= list2.size() - 1; k++) {
+				if (!(list.get(i).equals(list2.get(k)))) {
+					//System.out.println("Added to list: " + list.get(k));
+					int difference = 1;
+				}
+			}//end of inner for loop
+		}//end of outer for loop
+
+		return compareDifferentiatingWords;
+	}
 }
+
